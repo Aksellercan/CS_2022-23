42eabe7abf29a72e392cdb69eca78cee84cfd198
Aksellercan <118260347+Aksellercan@users.noreply.github.com>
2022-12-14 00:11:16 +0100

    Add files via upload

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+int c = 2 + 2;
+Console.WriteLine(c);
+Console.ReadLine();
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Width:");
+
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Height");
+
+int b = int.Parse(Console.ReadLine());
+
+int area = a * b;
+
+Console.WriteLine("Area of the rectangle is " + area);
+Console.ReadLine();
