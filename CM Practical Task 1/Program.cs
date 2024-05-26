3ec776f5fb8a6c6ca3851d88b8b0d56c982f6ade
aksell <118260347+Aksellercan@users.noreply.github.com>
2024-05-27 00:14:28 +0200

    Add files via upload

---
+++ CM Practical Task 1/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine(Convert("-59", 10, 2));
+        Console.WriteLine(Convert("101", 2, 10));
+        Console.WriteLine(Convert("101001011101", 2, 16)); 
+        Console.WriteLine(Convert("72312", 8, 8));
+
+        try
+        {
+            Console.WriteLine(Convert("92312", 8, 8)); 
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        } 
+        
+        try{
+        Console.WriteLine(Convert("0xA", 10, 8)); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static string Convert(string number, int sourceBase, int destBase)
+    {
+        if (!IsValidBase(sourceBase) || !IsValidBase(destBase))
+        throw new ArgumentException("Error (invalid base)");
+
+        bool isNegative = false;
+        if (number.StartsWith("-"))
+        {
+            isNegative = true;
+            number = number.Substring(1);
+        }
+        else if (number.StartsWith("0x"))
+        {
+            if (sourceBase != 16)
+                throw new ArgumentException("Error (source base ambiguous)");
+            number = number.Substring(2);
+        }
+
+        long decimalValue = ConvertToDecimal(number, sourceBase);
+
+        return ConvertFromDecimal(decimalValue, destBase, isNegative);
+    }
+
+    static long ConvertToDecimal(string number, int sourceBase)
+    {
+        long result = 0;
+        long power = 1;
+
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+
+            int digitValue = GetDigitValue(number[i]);
+
+            if (digitValue >= sourceBase)
+                throw new ArgumentException("Error (unrecognised  digit)");
+
+            result += digitValue * power;
+            power *= sourceBase;
+        }
+
+        return result;
+    }
+
+    static string ConvertFromDecimal(long decimalValue, int destBase, bool isNegative)
+    {
+        if (decimalValue == 0)
+            return "0";
+
+        string result = "";
+
+        while (decimalValue > 0)
+        {
+            long remainder = decimalValue % destBase;
+
+            result = GetDigit(remainder) + result;
+
+            decimalValue /= destBase;
+        }
+
+        if (isNegative)
+            result = "-" + result;
+
+    return result;
+    }
+
+    static int GetDigitValue(char digit)
+    {
+        if (char.IsDigit(digit))
+
+            return digit - '0';
+
+        else if (char.ToUpper(digit) >= 'A' && char.ToUpper(digit) <= 'F')
+
+        return char.ToUpper(digit) - 'A' + 10;
+
+            else
+
+                throw new ArgumentException("Unrecognized digit");
+    }
+
+    static string GetDigit(long value)
+    {
+        if (value >= 0 && value <= 9)
+
+            return value.ToString();
+
+
+        else if (value >= 10 && value <= 15)
+
+            return ((char)(value - 10 + 'A')).ToString();
+
+        else
+            throw new ArgumentException("Invalid digit value");
+    }
+
+    static bool IsValidBase(int numBase) { return numBase >= 2 && numBase <= 16; }
+
+}
+
+
+
+
---
+++ CM Practical Task 2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // RandInt(a, b) , RandFloat(a, b)
+
+        Console.WriteLine($"Random integer between (a) 10 and (b) 20: Random Integer {RandInt(10, 20 )}");
+        Console.WriteLine($"Random float between (a) 0 and (b) 1: Random float {RandFloat(0, 1)}");
+
+        double[] array = { 1.5, 2.5, 3.5, 4.5, 5.5 };
+
+        Console.WriteLine($"Random element from the array: Random element {RandElement(array)}");
+
+        Console.WriteLine($"Random string of length 10: Random string { RandString(10)} ");
+    }
+
+    static double Rand()
+    {
+        // variables:
+
+        //max value 
+        long m1 = 4294967087;       long m2 = 4294944443;
+
+        // used to scale and mix to generate neww random values
+        long a12 = 1403580;         long a13 = 810728;
+        long a21 = 527612;          long a23 = 1370589;
+
+        //additional parameters
+        long q12 = 53668;           long q13 = 52774;
+        long q21 = 12211;           long q23 = 3791;
+
+        //current state of algorithm
+        long r12 = 410184288;       long r13 = 3725392621;
+        long r21 = 2;               long r23 = 12345;
+
+        long h, p12, p13, p21, p23;
+        
+        //used in calculations
+        long h12, h13;
+
+        //used to  compute next state variables
+        long k;
+
+        //calculations
+        h = r12 / q13;
+        p13 = -a13 * (r13 - h * q13) + h * m1;
+        h = r13 / q13;
+        p12 = a12 * (r12 % q12) - a12 * (r12 / q12) * q12 - p13 + h * m1;
+       
+        if (p12 < 0) p12 += m1;
+
+        r12 = r13;   r13 = r21;
+
+        h = r12 / q12;
+
+        p21 = -a21 * (r21 - h * q21) + h * m2;
+
+        h = r21 / q12;
+
+        p23 = a23 * (r23 % q23) - a23 * (r23 / q23) * q23 - p21 + h * m2;
+
+        if (p23 < 0) p23 += m2;
+
+        r21 = r23;
+        r23 = r13;
+        if (p12 < p23) k = p12 - p23 + m1;
+
+        else k = p12 - p23;
+
+        if (k == 0) k = m1;
+
+        r12 = r12 % m1;     r13 = r13 % m2;
+        r21 = r21 % m1;     r23 = r23 % m2;
+
+        if (r12 < 0) r12 += m1;
+        if (r13 < 0) r13 += m2;
+        if (r21 < 0) r21 += m1;
+        if (r23 < 0) r23 += m2;
+        if (r12 > 0) r12 -= 1;
+
+        else r12 += (m1 - 1);
+
+        if (r13 > 0) r13 -= 1;
+
+        else r13 += (m2 - 1);
+
+        if (r21 > 0) r21 -= 1;
+
+        else r21 += (m1 - 1);
+
+        if (r23 > 0) r23 -= 1;
+
+        else r23 += (m2 - 1);
+
+        h12 = r12;
+        h13 = r13;
+
+        if (h12 < h13) k = h12 - h13 + m1;
+        else k = h12 - h13;
+
+        return (double)k / m1;
+    }
+
+    static int RandInt(int a, int b) { return (int) (a + (b - a + 1) * Rand()); }
+
+
+    static double RandFloat(double a, double b) { return a + (b - a) * Rand(); }
+
+
+    static double RandElement(double[] A)
+    {
+        int index = RandInt(0, A.Length - 1);
+
+            return A[index];
+    }
+
+    static string RandString(int a)
+    {
+     string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+
+        Random random = new Random();
+
+        char[] stringChars = new char[a];
+
+        for (int i = 0; i < stringChars.Length; i++) { stringChars[i] = chars[random.Next(chars.Length)]; }
+
+
+        return new string(stringChars);
+    }
+}
