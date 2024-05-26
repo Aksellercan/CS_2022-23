1a2e15a7ec113f86410a8f3872c0b8395ac00494
aksell <118260347+Aksellercan@users.noreply.github.com>
2024-05-27 00:18:57 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿/*int aa = 15;
+int bb = 10;
+Console.WriteLine($"The value of aa is: {aa} and the value of bb is: {bb}");
+*/
+double x = 2.55;
+double y = 3.77;
+double Total = x + y;
+Console.WriteLine($"The Total of x and y is {Total}");
